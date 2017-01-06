3d5ae42f17f6f43a8b0a98734237a7fb834fb79a
Mark Korondi <oro@zurich.ibm.com>
2017-01-06 21:56:18 +0100

    nmcli commands in wifi fix

--- bin/wifi-fix.sh
+++ bin/wifi-fix.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
+IFACE=wlp3s0
+
+nmcli device disconnect $IFACE
 sudo rmmod iwlmvm
 sudo rmmod iwlwifi
-sudo modprobe iwlwifi
+echo "Removed wireless modules"
 sleep 3
-sudo iwlist wlp3s0 s
+sudo modprobe iwlwifi
+echo "Added wireless modules"
+sleep 3
+sudo iwlist $IFACE s
