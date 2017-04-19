48e1fd5a0f407f4780724e36287e86b910ce1b2a
Mark Korondi <korondi.mark@gmail.com>
2017-04-19 23:48:31 +0200

    Minor modifications
    * Vivaldi default browser

--- bin/tmux-music.sh
+++ bin/tmux-music.sh
@@ -1,5 +1,7 @@
+#!/usr/bin/env bash
+
 WINDOW_ID=$(wmctrl -l | grep 'tmux.*Session' | grep $HOSTNAME | awk '{print $1}')
-echo "Window_ID: $WINDOW_ID"
+
 if [ ! -z "$WINDOW_ID" ]; then
     echo "exists"
     # Window exist, switch to it
