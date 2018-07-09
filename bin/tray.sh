bb3075e437cc9835c3d35ea49a83f426512b0e62
Mark Korondi <korondi.mark@gmail.com>
2018-07-09 10:56:53 +0100

    tray rearrangement

--- bin/tray.sh
+++ bin/tray.sh
@@ -15,7 +15,7 @@ sleep 0.5
 
 for try in {1..10}; do
     VALUES=($(xwininfo -root -tree \
-              | grep -i polybar \
+              | grep -i polybar-kmarc \
               | sed 's/^.*\ \([0-9]\+\)x\([0-9]\+\)[+-]\([0-9]\+\)[+-]\([0-9]\+\).*$/\1 \2 \3 \4/'))
     if [ -z "${VALUES[0]}" ]; then
         echo "Waiting for polybar ($try)"
@@ -30,8 +30,11 @@ BAR_HEIGHT=${VALUES[1]}
 BAR_X=${VALUES[2]}
 BAR_Y=${VALUES[3]}
 
-POS_X=$((BAR_WIDTH / 2 + BAR_X + 190))
+POS_X=$((BAR_WIDTH / 2 + BAR_X + 90))
 POS_Y=${BAR_Y}
+echo $BAR_WIDTH
+echo $BAR_X
+echo $POS_X  $POS_Y
 GEOMETRY="1x1+${POS_X}+${POS_Y}"
 REGION="${POS_X}x${POS_Y}+200+${BAR_HEIGHT}"
 
