379f66cedcaa6b8393ff497ea3f85436d4786014
Mark Korondi <korondi.mark@gmail.com>
2018-10-02 12:05:07 +0200

    sxhkd: removed lunxh, switch to xfce4-terminal

--- bin/lunch-migros.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-DOMAIN=https://www.migros.ch
-PAGE=$DOMAIN/de/genossenschaften/migros-zuerich/standorte/gastronomie/migros-restaurants.html
-PDF_PATH=$(curl "$PAGE" | awk -F'"' "/KW$(date +%W).2018.pdf/{ print \$2 }")
-
-evince "${DOMAIN}${PDF_PATH}"
