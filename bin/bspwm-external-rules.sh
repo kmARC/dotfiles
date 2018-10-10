f06f1460707f43757c25a6264a2a6b3199476cb3
Mark Korondi <korondi.mark@gmail.com>
2018-10-10 12:19:46 +0200

    bspwm: fixed rule for dialogs, config picks up theme

--- bin/bspwm-external-rules.sh
+++ bin/bspwm-external-rules.sh
@@ -7,13 +7,17 @@ class=$2
 instance=$3
 wmname=$(xprop -id "$wid" _NET_WM_NAME)
 
-echo "ID: $wid, class: $class, instance: $instance" >> /tmp/bspwm-debug.txt
-echo " \`--> type: $(xprop -id "$wid" _NET_WM_WINDOW_TYPE)" >> /tmp/bspwm-debug.txt
-echo " \`--> state: $(xprop -id "$wid" _NET_WM_STATE)" >> /tmp/bspwm-debug.txt
-echo " \`--> wmname: $wmname" >> /tmp/bspwm-debug.txt
+{
+  echo "ID: $wid, class: $class, instance: $instance"
+  echo " \`--> type: $(xprop -id "$wid" _NET_WM_WINDOW_TYPE)"
+  echo " \`--> state: $(xprop -id "$wid" _NET_WM_STATE)"
+  echo " \`--> wmname: $wmname"
+} >> /tmp/bspwm-debug.txt
 
 if [[ "$(xprop -id "$wid" _NET_WM_STATE)" == *"_NET_WM_STATE_ABOVE"* ]]; then
     echo "border = off state = floating"
+elif [[ "$(xprop -id "$wid" _NET_WM_WINDOW_TYPE)" == *"_NET_WM_WINDOW_TYPE_DIALOG"* ]]; then
+    echo "state = floating"
 elif [[ "$(xprop -id "$wid" _NET_WM_WINDOW_TYPE)" == *"_NET_WM_WINDOW_TYPE_NORMAL"* ]]; then
     echo "state = tiled"
 fi
