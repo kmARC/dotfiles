eba49992d27114ba77f76c7bc90967b6b76c74e1
Mark Korondi <korondi.mark@gmail.com>
2019-02-24 11:08:31 +0000

    polybar weather, icons, bluetooth updates

--- bin/polybar-bt.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ $(bluetoothctl show) =~ Powered:\ no ]]; then
-  echo "" # Bluetooth disabled
-elif [[ $(bluetoothctl info | grep -c 'Connected:.*yes') -eq 0 ]]; then
-  echo "" # Bluetooth enabled but not connected
-else
-  echo "" # Bluetooth connected
-fi
