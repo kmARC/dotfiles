4f7f4c6db19256ff72cf1483de911a85bb5ea568
Mark Korondi <korondi.mark@gmail.com>
2019-02-26 17:55:10 +0000

    bspwm: rules cleanup

--- bin/bspwm-external-rules.sh
+++ bin/bspwm-external-rules.sh
@@ -1,36 +1,31 @@
 #!/usr/bin/env bash
 
-#! /bin/sh
-
 wid=$1
 class=$2
 instance=$3
-wmname=$(xprop -id "$wid" _NET_WM_NAME)
 
-{
-  echo "ID: $wid, class: $class, instance: $instance"
-  echo " \`--> type: $(xprop -id "$wid" _NET_WM_WINDOW_TYPE)"
-  echo " \`--> state: $(xprop -id "$wid" _NET_WM_STATE)"
-  echo " \`--> wmname: $wmname"
-} >> /tmp/bspwm-debug.txt
+debug() { echo "$@" >> /tmp/bspwm-debug.txt; }
 
-if [[ "$(xprop -id "$wid" _NET_WM_STATE)" == *"_NET_WM_STATE_ABOVE"* ]]; then
-    echo "state = floating"
-elif [[ "$(xprop -id "$wid" _NET_WM_WINDOW_TYPE)" == *"_NET_WM_WINDOW_TYPE_DIALOG"* ]]; then
-    echo "state = floating"
-fi
+IFS=$'\n' props=($(xprop -id "$wid" _NET_WM_NAME \
+                                    _NET_WM_STATE \
+                                    _NET_WM_WINDOW_TYPE \
+                                    _MOTIF_WM_HINTS))
 
-if [[ $class =~ zoom && "$(xprop -id "$wid" _MOTIF_WM_HINTS)" == *"0x"?", 0x"?", 0x0, 0x"?", 0x"* ]]; then
-    # Zoom join call window
-    echo "border = off"
-fi
+debug "ID: $wid, class: $class, instance: $instance"
+debug " \`--> state:  ${props[1]}"
+debug " \`--> type:   ${props[2]}"
+debug " \`--> hints:  ${props[3]}"
+debug " \`--> wmname: ${props[0]}"
 
-if [[ $class =~ Slack ]]; then
-    if [[ "$wmname" == *"Call Minipanel"* ]]; then
-        echo "state = floating"
-    fi
+if   [[ "${props[1]}" == *"_NET_WM_STATE_ABOVE"*          ]]; then
+  echo "state = floating"
+elif [[ "${props[2]}" == *"_NET_WM_WINDOW_TYPE_DIALOG"*   ]]; then
+  echo "state = floating"
+elif [[ "${props[3]}" == *"0x"?", 0x"?", 0x0, 0x"?", 0x"* ]]; then
+  [[ $class =~ zoom ]]                     && echo "border = off"
+elif [[ $class =~ Slack ]]; then
+  [[ ${props[0],,} == *Call\ Minipanel* ]] && echo "state = floating"
 elif [ -z "$class" ]; then
-    if [[ $wmname =~ Spotify ]]; then
-      echo "desktop = 8 follow = on"
-    fi
+  [[ ${props[0],,} == *spotify* ]]         && echo "desktop = 8 follow = on"
 fi
+
