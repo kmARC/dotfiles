c0104ea64aaf76a033c447e8f721480a977deaf7
Mark Korondi <korondi.mark@gmail.com>
2020-01-03 20:45:28 +0100

    More robust backup script

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,115 +1,160 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -Eeuo pipefail
 
-DEST_S3_BUCKET="$USER-backup-2"
-DEST="${DEST:-/run/media/kmarc/Backup/Backup}"
-EXCLUDES="/tmp/backup_excludes.txt"
-LOCK_FILE="/tmp/lock.backup"
-
-if [ ! -d "$DEST" ]; then
-    >&2 echo "Destination directory ($DEST) not available."
-    >&2 echo "Exiting."
-    exit 255
-fi
-
-if [[ $# -gt 0 && "$1" == "synconly" ]]; then
-    aws --profile s3-backup s3 sync "${DEST}" "s3://${DEST_S3_BUCKET}"
-    exit 0
-fi
-
-cat > "$EXCLUDES" <<EOF
-$DEST
-$HOME/.cache
-$HOME/.bitcoin/blocks
-$HOME/.bitcoin/chainstate
-$HOME/.m2
-$HOME/.npm
-$HOME/.nvm
-$HOME/.rustup
-$HOME/.thumbnails
-$HOME/.tor-browser
-$HOME/.vagrant.d/boxes
-$HOME/.vim/vimswap
-$HOME/.vim/vimundo
-$HOME/.vim/vimbackup
-$HOME/Downloads
-$HOME/Torrents
-$HOME/VirtualBox VMs
-$HOME/vmware
-$HOME/.local/share/Trash
-$HOME/.local/share/Steam
-$HOME/.local/src
-**/*.bak
-**/*.ova
-**/*.pyc
-**/.notmuch
-**/__pycache__
-**/node_modules
-**/Cache
-**/tmp
-EOF
-
-if [ -f "$LOCK_FILE" ]; then
-    >&2 echo "Lock file exists at $LOCK_FILE. Running processes for $0:"
-    ps aux | grep "$0"
-    >&2 echo "Exiting."
-    exit 255
-fi
-
-BORG_PASSPHRASE=$(secret-tool lookup type borg_passphrase)
-export BORG_PASSPHRASE
-
-if [ -z "$BORG_PASSPHRASE" ]; then
-    >&2 echo "No password specified. Check your secrets. Seahorse, secret-tool"
-    >&2 echo "Exiting."
-    exit 255
-fi
-
-function cleanup {
-  rm -f "$LOCK_FILE" "$EXCLUDES";
-  exit $?
+usage() {
+  echo "Usage: $0 (backup|synconly|list|mount|repair)"
+  echo "          [-c config] [-d destination] [-p aws profile] [-b s3-bucket]"
+  echo "          -s: sync to s3"
+  exit
 }
 
-touch "$LOCK_FILE"
-trap cleanup TERM EXIT
+check() {
+  if [ -f "$LOCK_FILE" ]; then
+      >&2 echo "Lock file exists at $LOCK_FILE. Running processes for $0:"
+      pgrep -f "$0"
+      >&2 echo "Exiting."
+      exit 1
+  fi
 
-echo "======"
-echo "    Backup script starting @ $(date) - $HOME => $DEST"
-echo "======"
+  if [ -z "$BORG_PASSPHRASE" ]; then
+      >&2 echo "No password specified. Check your secrets. Seahorse, secret-tool"
+      >&2 echo "Exiting."
+      exit 1
+  fi
 
-if [ -f "$DEST"/README ]; then
-    borg create --verbose \
-                --progress \
-                --stats \
-                --compression auto,lzma \
-                --exclude-from "$EXCLUDES" \
-                "$DEST"::'{now}' \
-                "$HOME"
+  if [ ! -d "$DEST" ]; then
+      >&2 echo "Destination directory ($DEST) not available."
+      >&2 echo "Exiting."
+      exit 1
+  fi
 
-    borg prune --keep-within 1d \
-               --keep-daily 7 \
-               --keep-weekly 4 \
-               --keep-monthly 12 \
-               --keep-yearly 1 \
-               --save-space \
-               "$DEST"
+  if [ ! -f "$DEST"/README ]; then
+      >&2 echo "Destination directory ($DEST) doesn't look like a Borg repository."
+      >&2 echo "Initialize first with 'borg init'."
+      >&2 echo "Exiting."
+      exit 1
+  fi
+}
 
-    borg list "$DEST"
-fi
+backup() {
+  # Create excludes file, add larger than 1G files
+  printf '%s\n' "${EXCLUDES[@]}" > "$EXCLUDES_FILE"
+  (find "$HOME" -type f -size +1G 2>/dev/null || true) >> "$EXCLUDES_FILE"
 
-sync
+  touch "$LOCK_FILE"
 
-echo
-if [[ $# -gt 0 && "$1" == "sync" ]]; then
-    echo "======"
-    echo "    Syncing to s3://${DEST_S3_BUCKET}"
-    echo "======"
-    aws --profile s3-backup s3 sync "${DEST}" "s3://${DEST_S3_BUCKET}"
-else
-    echo "To sync to s3:"
-    echo "    $0 synconly"
-fi
-echo
-echo "======"
-echo "    Backup script exiting @ $(date)."
-echo "======"
+  echo "======"
+  echo "    Backup script starting @ $(date) - $HOME => $DEST"
+  echo "======"
+  borg break-lock "$DEST"
+  borg create     --verbose \
+                  --progress \
+                  --stats \
+                  --compression auto,lzma \
+                  --exclude-from "$EXCLUDES_FILE" \
+                  "$DEST"::'{now}' \
+                  "$HOME"
+  sync
+  borg prune      --keep-within 1d \
+                  --keep-daily 7 \
+                  --keep-weekly 4 \
+                  --keep-monthly 12 \
+                  --keep-yearly 1 \
+                  --save-space \
+                  "$DEST"
+  borg list       "$DEST"
+  echo "======"
+  echo "    Backup finished at $(date)."
+  echo "======"
+
+  # Sync to s3 if needed
+  [ "$SYNC" = true ] && sync_to_s3
+}
+
+list() {
+  borg list "$DEST"
+  borg info "$DEST"
+}
+
+mount_repo() {
+  MOUNT_DIR=$(mktemp -d /tmp/backup.mount.XXX)
+  borg mount "$DEST" "$MOUNT_DIR"
+  echo "======"
+  echo "    Backup repository mounted at '$MOUNT_DIR'"
+  echo "    To unmount:"
+  echo "        fusermount -u $MOUNT_DIR"
+  echo "======"
+}
+
+sync_to_s3() {
+  echo "======"
+  echo "    Syncing to s3://$S3_BUCKET"
+  echo "======"
+  aws --profile "$AWS_PROFILE" s3 sync "$DEST" "s3://$S3_BUCKET"
+}
+
+repair() {
+  echo "======"
+  echo "    Repair '$DEST'"
+  echo "======"
+  borg check --repair "$DEST"
+  echo "======"
+  echo "    Done."
+  echo "======"
+}
+
+cleanup() {
+  rm -f "$LOCK_FILE" "$EXCLUDES_FILE"
+}
+
+# Error handling
+trap "cleanup" INT TERM HUP EXIT
+trap "echo Error in: \${FUNCNAME:-top level}, line \${LINENO}" ERR
+
+##########
+#  Main  #
+##########
+
+# Save action and shift to the flags
+ACTION=${1:-}
+[ -n "$ACTION" ] && shift
+
+# Parse configuration
+while getopts 'c:d:b:p:s' c; do
+  case $c in
+    c) CONFFILE=$OPTARG ;;
+    s) SYNC=true ;;
+    d) _DEST=$OPTARG ;;
+    b) _S3_BUCKET=$OPTARG ;;
+    p) _AWS_PROFILE=$OPTARG ;;
+    *) ;;
+  esac
+done
+
+# Set configuration
+CONFFILE=${CONFFILE:-$HOME/.config/backuprc}
+# shellcheck source=/dev/null
+source "$CONFFILE" || (>&2 echo "File '$CONFFILE' cannot be opened" && exit 1)
+# Override configuration
+DEST=${DEST:-$_DEST}
+AWS_PROFILE=${AWS_PROFILE:-$_AWS_PROFILE}
+S3_BUCKET=${S3_BUCKET:-$_S3_BUCKET}
+EXCLUDES=${EXCLUDES:-}
+# Opts
+SYNC=${SYNC:-false}
+# Defaults
+EXCLUDES_FILE=$(mktemp /tmp/backup.excludes.XXXXX.txt)
+LOCK_FILE="/tmp/backup.lock"
+
+# Check if all configuration needed is properly set
+check
+
+# Invoke action
+case "$ACTION" in
+  'backup')   backup ;;
+  'synconly') sync_to_s3 ;;
+  'list')     list ;;
+  'mount')    mount_repo ;;
+  'repair')   repair ;;
+  *)          usage ;;
+esac
