0746ee764210f0de714e3478d0922714a9da76b9
Mark Korondi <korondi.mark@gmail.com>
2020-01-03 20:50:17 +0100

    bspwm external rules updated

--- bin/bspwm-external-rules.sh
+++ bin/bspwm-external-rules.sh
@@ -18,12 +18,26 @@ debug " \`--> hints:  ${props[3]}"
 debug " \`--> wmname: ${props[0]}"
 
 if   [[ $class == Slack ]]; then
+  debug "case 1"
   [[ ${props[0],,} == *call\ minipanel* ]] && echo "state = floating sticky = true"
-elif [[ ${props[1]} == *"_NET_WM_STATE_ABOVE"*          ]]; then
+elif [[ ${props[1]} == *_SKIP_TASKBAR*_SKIP_PAGER* ]]; then
+  debug "case 2"
+  echo "border = false"
+elif [[ ${props[1]} == *_STATE_ABOVE*              ]]; then
+  debug "case 3"
   echo "state = floating"
-elif [[ ${props[2]} == *"_NET_WM_WINDOW_TYPE_DIALOG"*   ]]; then
+  echo "border = false"
+elif [[ ${props[2]} == *_TYPE_DIALOG*              ]]; then
+  debug "case 4"
   echo "state = floating"
+elif [[ "${props[3]}" =~ 0x3,\ 0x.,\ 0x0,\ 0x.,\ 0x. ]]; then
+  debug "case 5"
+  echo "state = floating"
+elif [[ "${props[0]}" == *Loading\ Microsoft\ Teams* ]]; then
+  debug "case 6"
+  echo "border = true"
 elif [ -z "$class" ]; then
+  debug "case 7"
   [[ ${props[0],,} == *spotify* ]] && echo "desktop = 8 follow = on"
 fi
 
