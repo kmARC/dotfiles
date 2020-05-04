ee0e8844a0e28c1f8f3a37b72c8b4047770e9013
Mark Korondi <mark.korondi@k-lab.ch>
2020-05-04 18:45:57 +0200

    Removed ranger rc, added fodler hook for imap

--- config/redshift/hooks/hook.sh
+++ config/redshift/hooks/hook.sh
@@ -5,14 +5,14 @@ case $1 in
         notify-send "Redshift" "Period changed from $2 to $3"
 
         case $3 in
-          daytime|none|transition)
+          daytime|none)
             gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita'
             sed --follow-symlinks -i "s/^gtk-theme-name=.*/gtk-theme-name=Adwaita/g"      $HOME/.config/gtk-3.0/settings.ini
             sed --follow-symlinks -i "s/^Net\/ThemeName.*/Net\/ThemeName \"Adwaita\"/g"   $HOME/.xsettingsd ; pkill xsettingsd
             sed --follow-symlinks -i "s/^gtk-theme-name=.*/gtk-theme-name=\"Adwaita\"/g"  $HOME/.gtkrc-2.0
             sed --follow-symlinks -i "s/^colors:\s\+\*\\(light\|dark\\)/colors: *light/g" $HOME/.config/alacritty.yml
             ;;
-          night)
+          night|transition)
             gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
             sed --follow-symlinks -i "s/^gtk-theme-name=.*/gtk-theme-name=Adwaita-dark/g"     $HOME/.config/gtk-3.0/settings.ini
             sed --follow-symlinks -i "s/^Net\/ThemeName.*/Net\/ThemeName \"Adwaita-dark\"/g"  $HOME/.xsettingsd ; pkill xsettingsd
