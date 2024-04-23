800482d09b809a2122121bda0b2a8d028538e971
Mark Korondi <korondi.mark@gmail.com>
2024-04-23 17:44:53 +0200

    2024 April updates

--- bin/colors_and_formatting.sh
+++ bin/colors_and_formatting.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # This porgram is free software.  It comes without any warranty, to 
 # the extent permitted by applicable law.  You can redistribute it 
@@ -18,5 +18,12 @@ for clbg in {40..47} {100..107} 49; do
         echo #New line
     done
 done
+
+echo -e '\e[1mBold\e[22m'
+echo -e '\e[2mDimmed\e[22m'
+echo -e '\e[3mItalic\e[23m'
+echo -e '\e[4mUnderlined\e[24m'
+echo -e '\e[4:3mCurly Underlined\e[4:0m'
+echo -e '\e[4:3m\e[58;2;240;143;104mColored Curly Underlined\e[59m\e[4:0m'
 
 exit 0
